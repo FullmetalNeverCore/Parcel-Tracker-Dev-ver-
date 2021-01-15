acd6e8a0e9c16a5b24e6be4cda0c64a12a1e9261
FullmetalNeverCore <60969891+FullmetalNeverCore@users.noreply.github.com>
2021-01-15 04:26:55 +0200

    updt 0.3

---
+++ teletrack1.py
@@ -0,0 +1,28 @@
+import aiogram
+from aiogram import Bot, Dispatcher, executor, md, types
+import os 
+import sys 
+import logging 
+import trackparcel 
+from trackparcel import *
+
+
+token = ""
+api = Bot(token=token, parse_mode=types.ParseMode.MARKDOWN)
+disp = Dispatcher(api)
+logging.basicConfig(level=logging.INFO)
+
+@disp.message_handler(commands=['track'])
+async def track(message: types.Message):
+    id = message.from_user.id
+    mt = message.text 
+    print(message.text)
+    mt2 = mt.split("-")
+    await trackparcel.url_creator(mt2[1], id)
+
+async def send_msg(msg, id):
+    print(msg)
+    await api.send_message(int(id), msg)
+
+if __name__ == '__main__':
+    executor.start_polling(disp, skip_updates=True)
